ac7f4cf56d94e8711d2733825ce5ec837576b044
Stephen Hodgson <rage.against.the.pixel@gmail.com>
2024-10-02 10:00:29 -0400

    .

--- dist/index.js
+++ dist/index.js
@@ -33233,8 +33233,8 @@ async function installWindows(version) {
     const [url, archiveName] = await getDownloadUrl(version);
     core.info(`Downloading ${archiveName} from ${url}...`);
     const installerPath = path.join(getTempDirectory(), archiveName);
-    const downloadPath = await tc.downloadTool(url, installerPath);
-    await exec.exec('pwsh', ['-Command', `Start-Process -Wait -FilePath "${downloadPath}" -ArgumentList '--mode unattended --unattendedmodeui none --disable-components ideintegrations,eclipse,mylyn,intellij12'`]);
+    await exec.exec('pwsh', ['-Command', `Invoke-WebRequest -Uri "${url}" -OutFile "${installerPath}"`]);
+    await exec.exec('pwsh', ['-Command', `Start-Process -Wait -FilePath "${installerPath}" -ArgumentList '--mode unattended --unattendedmodeui none --disable-components ideintegrations,eclipse,mylyn,intellij12'`]);
 }
 async function installMac(version) {
     if (!version) {
